43ac20edda6231ff7a6c48d65906ff445db1db4d
ebikatsu <ennir0508@gmail.com>
2025-12-28 19:30:31 +0900

    feat: デバッグ用設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	build: {
+		sourcemap: true, // ビルド後のデバッグが必要な場合
+	},
 });
